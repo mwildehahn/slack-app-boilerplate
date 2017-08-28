1df0b38cb31c345c0d2b5024892e3e82c1e09aa9
Michael Hahn <mh@slack-corp.com>
2017-08-27 18:35:24 -0700

    Update to work with new material-ui api

--- components/with-root.js
+++ components/with-root.js
@@ -2,12 +2,11 @@ import { Component } from "react";
 import { JssProvider } from "react-jss";
 import {
   withStyles,
-  createStyleSheet,
   MuiThemeProvider
 } from "material-ui/styles";
 import { getContext } from "../styles/context";
 
-const styleSheet = createStyleSheet(theme => ({
+const styles = theme => ({
   "@global": {
     html: {
       background: theme.palette.background.default,
@@ -18,11 +17,11 @@ const styleSheet = createStyleSheet(theme => ({
       margin: 0
     }
   }
-}));
+});
 
 let AppWrapper = props => props.children;
 
-AppWrapper = withStyles(styleSheet)(AppWrapper);
+AppWrapper = withStyles(styles)(AppWrapper);
 
 function withRoot(ComposedComponent) {
   class WithRoot extends Component {
